1ef91a490d8ccc6c7d549d24665ef16b1198db2d
Sharjeel-Asif <hamza2024don@gmail.com>
2024-02-09 15:42:37 +0500

    Add Files

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-let note_Container=document.querySelector(".note-container")
-let addBtn=document.querySelector("#addBtn")
+let notification_container=document.querySelector("#notification")
 
 
-window.addEventListener("load",()=>{
-  let savedNotes=localStorage.getItem("notes")
-  if(saveNotes){
- note_Container.innerHTML=savedNotes
-  }
-})
-addBtn.addEventListener("click",()=>{
-  let p=document.createElement("p")
-  p.setAttribute("contenteditable","true")
-  note_Container.appendChild(p)
-  p.classList.add("style")
-  let img=document.createElement("img")
-  img.src="./trash.png"
-  p.appendChild(img)
-  img.classList.add("move")
-  img.addEventListener("click", (e) => {
-    e.currentTarget.parentElement.remove();
-    saveNotes()
+let buttons=document.querySelectorAll("#button")
+
+buttons.forEach((button)=>{
+  button.addEventListener("click",(e)=>{
+ let notification=document.createElement("div")
+    notification.classList.add("toast")
+
+    notification_container.appendChild(notification)
+
+    let icon=document.createElement("i")
+    icon.classList.add("fa-solid","fa-circle-check")
+    if(e.target.innerHTML==="Success"){
+      notification.innerHTML="Suceessfully submitted"
+      notification.prepend(icon)
+      icon.classList.add("succes")
+    }else if(e.target.innerHTML==="Error"){
+      notification.innerHTML="Please fix the error"
+      notification.prepend(icon)
+      icon.classList.add("error","fa-solid","fa-circle-xmark")
+
+    }else{
+      notification.innerHTML="Invalid input, check again"
+      notification.prepend(icon)
+      icon.classList.add("invalid","fa-solid","fa-triangle-exclamation")}
+
+      setTimeout(()=>{
+        notification.remove()
+      },6000)
+    }
+    )
   })
-  
-  saveNotes()
-  })
-function saveNotes(){
-  localStorage.setItem("notes",note_Container.innerHTML)
-}
+
+
 
