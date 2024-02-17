305ecb710637c3e99b12136552e821ac0113bc7c
Sharjeel-Asif <hamza2024don@gmail.com>
2024-02-17 11:00:48 +0500

    Add files

--- script.js
+++ script.js
@@ -1,37 +1,105 @@
-let notification_container=document.querySelector("#notification")
+  let quoteApi="https://api.quotable.io/random"
+  let quoteDisplay=document.querySelector("#quote-Display")
+  let quoteInput=document.querySelector("#quoteInput")
+  const timer=document.querySelector("#timer")
+
+let correct=true
+quoteInput.addEventListener("input",()=>{
+  const characterSpan=document.querySelectorAll("span")
+  const quoteInputnValue=quoteInput.value.split("");
+  characterSpan.forEach((character,index)=>{
+    console.log(character)
+const  newCharacter = quoteInputnValue[index];
+console.log(newCharacter)
+if(newCharacter==null){
+  character.classList.remove("correct")
+  character.classList.remove("incorrect")
+  correct=false
+}
+else if(newCharacter===character.innerHTML){
+  character.classList.add("correct")
+  character.classList.remove("incorrect")
+
+  
+}else{
+  character.classList.remove("correct")
+  character.classList.add("incorrect")
+  correct=false
+} 
+})
+if(correct) renderNewQuote()
+    // console.log("d")
+  console.log(quoteInputnValue)
+
+})
+function getRandomQuote(){
+  return fetch(quoteApi)
+  .then((res)=> res.json())
+  .then(data => data.content)
+}
+
+async function renderNewQuote(){
+  const quote=await getRandomQuote()
+  quoteDisplay.innerHTML=""
+  quote.split("").forEach(currElem => {
+    const span=document.createElement("span")
+    span.innerText=currElem
+    // span.classList.add("correct")
+    quoteDisplay.appendChild(span)
+    // console.log(span)  
+  });
+  console.log(quote)
+  quoteInput.value=null
+  runTimer()
+}
+
+renderNewQuote()
 
 
-let buttons=document.querySelectorAll("#button")
+let startTime;
 
-buttons.forEach((button)=>{
-  button.addEventListener("click",(e)=>{
- let notification=document.createElement("div")
-    notification.classList.add("toast")
+function runTimer(){
 
-    notification_container.appendChild(notification)
+  timer.innerHTML=0
+  startTime=new Date()
+  setInterval(()=>{
+    timer.innerHTML= getTime()
 
-    let icon=document.createElement("i")
-    icon.classList.add("fa-solid","fa-circle-check")
-    if(e.target.innerHTML==="Success"){
-      notification.innerHTML="Suceessfully submitted"
-      notification.prepend(icon)
-      icon.classList.add("succes")
-    }else if(e.target.innerHTML==="Error"){
-      notification.innerHTML="Please fix the error"
-      notification.prepend(icon)
-      icon.classList.add("error","fa-solid","fa-circle-xmark")
+  },1000)
+}
 
-    }else{
-      notification.innerHTML="Invalid input, check again"
-      notification.prepend(icon)
-      icon.classList.add("invalid","fa-solid","fa-triangle-exclamation")}
 
-      setTimeout(()=>{
-        notification.remove()
-      },6000)
-    }
-    )
-  })
+function getTime(){
+ return Math.floor(( new Date() - startTime) / 1000)
+}
+
+
+console.log(getTime)
 
 
 
+
+
+
+  // console.log(quoteApi)
+//   function renderQuote(){
+//     return fetch(quoteApi)
+//     .then(response =>response.json())
+//     .then(data=>data.content)
+
+//   }
+
+//   async function getQuote(){
+//       const quote=await renderQuote()
+//       console.log(quote)
+//     quoteDisplay.innerHTML=""
+//       quote.split("").forEach((curr)=>{
+//       const span=document.createElement("span")
+//       span.innerHTML=curr + ""
+//       quoteDisplay.appendChild(span)
+//       console.log(span)
+//       })
+// quoteInput.innerHTML=""
+    
+//   }
+//   getQuote()
